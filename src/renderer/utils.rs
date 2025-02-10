8707c6b1ba4387010f4c3116b28490ec195ed9b6
qknight <19447200-qknight@users.noreply.replit.com>
2025-02-10 12:23:09 +0000

    fixing the date output function

--- src/renderer/utils.rs
+++ src/renderer/utils.rs
@@ -1,6 +1,6 @@
 pub fn date_and_time(modification_date: &Option<chrono::NaiveDateTime>) -> String {
     match modification_date {
-        Some(modification_date) => modification_date.format("%Y-%m-%d %H:%M").to_string(),
+        Some(modification_date) => modification_date.format("%d %b %Y").to_string().to_lowercase(),
         None => String::new(),
     }
 }
