84d3b7254d6bb8daeff30f2fc84d2a97e62e2b6a
qknight <19447200-qknight@users.noreply.replit.com>
2025-03-26 02:26:22 +0000

    flake cleanup

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
-# please read flake introduction here:
-# https://fasterthanli.me/series/building-a-rust-service-with-nix/part-10#a-flake-with-a-dev-shell
 {
-  description = "The fairsync importer prototype flake";
+  description = "pankat static blog generator";
   inputs = {
     rust-overlay.url = "github:oxalica/rust-overlay";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -15,7 +13,7 @@
           pkgs = import nixpkgs {
             inherit system overlays;
           };
-          rust = pkgs.rust-bin.fromRustupToolchainFile ./pankat-wasm/rust-toolchain.toml;
+          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           platform_packages =
             if pkgs.stdenv.isLinux then
               with pkgs; [ ]
@@ -30,11 +28,6 @@
         in
         with pkgs;
         rec {
-          #trunk = pkgs.callPackage ./trunk.nix {
-          #  inherit (darwin.apple_sdk.frameworks) CoreServices Security SystemConfiguration;
-          #};
-          #leptosfmt = pkgs.callPackage ./leptosfmt.nix {};
-
           devShells.default = mkShell {
             buildInputs = [
               cargo
